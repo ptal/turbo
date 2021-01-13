9dc318fa5c9ae305a0f56e079116fcf038c66d1e
Frederic Pinel <frederic.pinel@uni.lu>
2021-01-13 14:27:07 +0100

    simple cuda kernel.

--- src/main.cu
+++ src/main.cu
@@ -12,14 +12,18 @@
 
 #define CUDIE0() CUDIE(cudaGetLastError())
 
+template<typename T>__device__ __host__ T min(T a, T b) { return a<=b ? a : b; }
+template<typename T>__device__ __host__ T max(T a, T b) { return a>=b ? a : b; }
+
 struct Interval {
   int lb;
   int ub;
 };
 
-inline void join(Interval* a, Interval b) {
-  a->lb = std::max(a->lb, b.lb);
-  a->ub = std::min(a->ub, b.ub);
+__host__ __device__
+void join(Interval* a, Interval b) {
+  a->lb = max<int>(a->lb, b.lb);
+  a->ub = min<int>(a->ub, b.ub);
 }
 
 struct VStore {
@@ -27,10 +31,14 @@ struct VStore {
   size_t size;
 };
 
-VStore new_vstore(size_t nvar) {
+VStore* new_vstore(size_t nvar) {
+  VStore *vsd;
+  CUDIE(cudaMallocManaged(&vsd, sizeof(*vsd)));
   Interval* data;
   CUDIE(cudaMallocManaged(&data, sizeof(Interval) * nvar));
-  return {data, nvar};
+  vsd->data = data;
+  vsd->size = nvar;
+  return vsd;
 }
 
 typedef size_t Var;
@@ -47,20 +55,26 @@ void dom(VStore vstore, Var x, Interval itv) {
 }
 
 // x + y <= c
-void x_plus_y_leq_c(VStore vstore, Var x, Var y, int c)
+__global__
+void x_plus_y_leq_c(VStore* vstore, Var x, Var y, int c)
 {
-  join(&vstore.data[x], {vstore.data[x].lb, c - vstore.data[y].lb});
-  join(&vstore.data[y], {vstore.data[y].lb, c - vstore.data[x].lb});
+  join(&vstore->data[x], {vstore->data[x].lb, c - vstore->data[y].lb});
+  join(&vstore->data[y], {vstore->data[y].lb, c - vstore->data[x].lb});
 }
 
+__device__ VStore vstore_d;
+
 int main() {
-  VStore vstore = new_vstore(2);
+  VStore* vstore = new_vstore(2);
   int x = 0;
   int y = 1;
-  dom(vstore, x, {0, 2});
-  dom(vstore, y, {1, 3});
-  print_store(vstore);
-  x_plus_y_leq_c(vstore, x, y, 2);
-  print_store(vstore);
+  dom(*vstore, x, {0, 2});
+  dom(*vstore, y, {1, 3});
+  print_store(*vstore);
+  x_plus_y_leq_c<<<1,1>>>(vstore, x, y, 2);
+  CUDIE0();
+  CUDIE(cudaDeviceSynchronize());
+  // page fault expected:
+  print_store(*vstore);
   return 0;
 }
