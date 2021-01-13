cc04c14dec07f801a84c1fe8a14fa19db4497226
Pierre Talbot <ptalbot@hyc.io>
2021-01-13 14:13:03 +0100

    [constraints] Refactor into classes constraints and store.

--- src/main.cu
+++ src/main.cu
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <algorithm>
 #include <stdio.h>
+#include <cmath.h>
 
 #define CUDIE(result) { \
         cudaError_t e = (result); \
@@ -12,46 +13,95 @@
 
 #define CUDIE0() CUDIE(cudaGetLastError())
 
+typedef size_t Var;
+
 struct Interval {
   int lb;
   int ub;
-};
 
-inline void join(Interval* a, Interval b) {
-  a->lb = std::max(a->lb, b.lb);
-  a->ub = std::min(a->ub, b.ub);
-}
+  inline void join(Interval b) {
+    lb = max(lb, b.lb);
+    ub = min(ub, b.ub);
+  }
+
+  bool operator==(int x) {
+    return lb == x && ub == x;
+  }
+};
 
 struct VStore {
   Interval* data;
   size_t size;
+
+  VStore(int nvar) {
+    size = nvar;
+    CUDIE(cudaMallocManaged(&data, sizeof(Interval) * nvar));
+  }
+
+  void print_store() {
+    for(int i=0; i < size; ++i) {
+      printf("%d = [%d..%d]\n", i, data[i].lb, data[i].ub);
+    }
+  }
+
+  // lb <= x <= ub
+  void dom(Var x, Interval itv) {
+    data[x] = itv;
+  }
+
+  Interval& operator[](const size_t i) {
+    return data[i];
+  }
 };
 
-VStore new_vstore(size_t nvar) {
-  Interval* data;
-  CUDIE(cudaMallocManaged(&data, sizeof(Interval) * nvar));
-  return {data, nvar};
-}
+/// x + y <= c
+struct XplusYleqC {
+  Var x;
+  Var y;
+  int c;
 
-typedef size_t Var;
+  XplusYleqC(Var x, Var y, int c) : x(x), y(y), c(c) {}
 
-void print_store(VStore vstore) {
-  for(int i=0; i < vstore.size; ++i) {
-    printf("%d = [%d..%d]\n", i, vstore.data[i].lb, vstore.data[i].ub);
+  void propagate(VStore vstore)
+  {
+    vstore[x].join({vstore[x].lb, c - vstore[y].lb});
+    vstore[y].join({vstore[y].lb, c - vstore[x].lb});
   }
-}
 
-// lb <= x <= ub
-void dom(VStore vstore, Var x, Interval itv) {
-  vstore.data[x] = itv;
-}
+  bool is_entailed(VStore vstore) {
+    return vstore[x].ub + vstore[y].ub <= c;
+  }
 
-// x + y <= c
-void x_plus_y_leq_c(VStore vstore, Var x, Var y, int c)
-{
-  join(&vstore.data[x], {vstore.data[x].lb, c - vstore.data[y].lb});
-  join(&vstore.data[y], {vstore.data[y].lb, c - vstore.data[x].lb});
-}
+  bool is_disentailed(VStore vstore) {
+    return vstore[x].lb + vstore[y].lb > c;
+  }
+};
+
+// /// b <=> left /\ right
+// struct ReifiedLogicalAnd {
+//   Var b;
+//   XplusYleqC left;
+//   XplusYleqC right;
+
+//   ReifiedLogicalAnd(Var b, XplusYleqC left, XplusYleqC right) :
+//     b(b), left(left), right(right) {}
+
+//   void propagate(VStore vstore) {
+//     if vstore[b] == 0 {
+
+//     }
+//     else if vstore[b] == 1 {
+//       left.propagate(vstore);
+//       right.propagate(vstore);
+//     }
+//     else if left.is_entailed(vstore) && right.is_entailed(vstore) {
+//       vstore[b] = 1;
+//     }
+//     else if left.is_disentailed(vstore) && right.is_disentailed(vstore) {
+//       vstore[b] = 0;
+//     }
+//   }
+// }
 
 int main() {
   VStore vstore = new_vstore(2);
