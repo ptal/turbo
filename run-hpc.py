2194a318dd49ac681de33fa7a061d901e90c438f
Pierre Talbot <ptalbot@hyc.io>
2022-02-21 22:49:03 +0100

    Try to fix the newline problem.

--- run-hpc.py
+++ run-hpc.py
@@ -34,7 +34,7 @@ for filename in sorted_dir:
     instance_name = os.path.splitext(filename)[0]
     instance_path = directory_str + filename
     error_path = error_dir + instance_name + ".err"
-    os.system("echo \"\n" + instance_name + ", \" >> " + results_filename)
+    os.system("echo \"\n" + instance_name + ", \" | tee -a " + results_filename)
     command = "./turbo " + str(timeout) + " -and " + and_option + " -or " + or_option + " -sub " + sub_option + " " + instance_path + " 2> " + error_path + " | tee -a " + results_filename
     # print(command)
     os.system(command)
