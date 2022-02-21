b7fa53282c5ae7fad95a5dfd4442f27a7f4c566a
Pierre Talbot <ptalbot@hyc.io>
2022-02-21 22:38:05 +0100

    natural sorting on the CSV results.

--- run-hpc.py
+++ run-hpc.py
@@ -1,6 +1,13 @@
 import os
 import sys
+import re
 
+def natural_sort(l):
+    convert = lambda text: int(text) if text.isdigit() else text.lower()
+    alphanum_key = lambda key: [convert(c) for c in re.split('([0-9]+)', key)]
+    return sorted(l, key=alphanum_key)
+
+timeout = 300
 sub_option = sys.argv[1]
 suite = sys.argv[2]
 and_option = str(108*4)
@@ -10,22 +17,24 @@ if len(sys.argv) > 3:
 if len(sys.argv) > 4:
   or_option = sys.argv[4]
 
-directory = os.fsencode("benchmarks-xml/" + suite + "-xml/")
+directory_str = "benchmarks-xml/" + suite + "-xml/"
+directory = os.fsencode(directory_str)
 results_prefix = "benchmarks-xml/" + suite + "-" + and_option + "-" + or_option + "-" + sub_option
 results_filename = results_prefix + ".csv"
 with open(results_filename, 'w') as f:
   f.write("name, nodes, fails, solutions, depthmax, variables, constraints, satisfiability, exhaustivity, time, optimum")
 
 error_dir = results_prefix + "-err/"
-os.system("mkdir " + error_dir)
+os.system("mkdir -p " + error_dir)
 
-for file in os.listdir(directory):
-  filename = os.fsdecode(file)
+sorted_dir = natural_sort([os.fsdecode(file) for file in os.listdir(directory)])
+
+for filename in sorted_dir:
   if filename.endswith(".xml"):
     instance_name = os.path.splitext(filename)[0]
-    instance_path = os.fsdecode(os.path.join(directory, file))
+    instance_path = directory_str + filename
     error_path = error_dir + instance_name + ".err"
-    os.system("echo \"" + instance_name + ", \" >> " + results_filename)
-    command = "./turbo -and " + and_option + " -or " + or_option + " -sub " + sub_option + " " + instance_path + " 2> " + error_path + " | tee -a " + results_filename
-    # print(command)
-    os.system(command)
+    # os.system("echo \"\n" + instance_name + ", \" >> " + results_filename)
+    command = "./turbo " + str(timeout) + " -and " + and_option + " -or " + or_option + " -sub " + sub_option + " " + instance_path + " 2> " + error_path + " | tee -a " + results_filename
+    print(command)
+    # os.system(command)
