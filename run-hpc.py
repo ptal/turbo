692f57a796beb7e56caad56348e2734cc842944a
Pierre Talbot <ptalbot@hyc.io>
2022-02-21 22:13:59 +0100

    Fix Python script.

--- run-hpc.py
+++ run-hpc.py
@@ -16,12 +16,15 @@ results_filename = results_prefix + ".csv"
 with open(results_filename, 'w') as f:
   f.write("name, nodes, fails, solutions, depthmax, variables, constraints, satisfiability, exhaustivity, time, optimum")
 
+error_dir = results_prefix + "-err/"
+os.system("mkdir " + error_dir)
+
 for file in os.listdir(directory):
   filename = os.fsdecode(file)
   if filename.endswith(".xml"):
     instance_name = os.path.splitext(filename)[0]
     instance_path = os.fsdecode(os.path.join(directory, file))
-    error_path = results_prefix + "-err/" + instance_name + ".err"
+    error_path = error_dir + instance_name + ".err"
     os.system("echo \"" + instance_name + ", \" >> " + results_filename)
     command = "./turbo -and " + and_option + " -or " + or_option + " -sub " + sub_option + " " + instance_path + " 2> " + error_path + " | tee -a " + results_filename
     # print(command)
