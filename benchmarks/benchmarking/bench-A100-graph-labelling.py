ddfbb050242256bef5f5186d4b243cbd57d24948
Pierre Talbot <pierre.talbot@uni.lu>
2024-01-16 11:07:53 +0100

    Benchmarking of graph labelling on A100.

---
+++ benchmarks/benchmarking/bench-A100-graph-labelling.py
@@ -0,0 +1,15 @@
+from datetime import timedelta
+from pathlib import Path
+
+import minizinc
+
+from mzn_bench import Configuration, schedule
+
+schedule(
+    instances=Path("graph-labelling-pos64.csv"),
+    output_dir=Path("../campaign/turbo-v1.2.0-A100-64/"),
+    timeout=timedelta(seconds=600),
+    debug=False,
+    configurations= [Configuration(name=f"TurboGPU", solver=minizinc.Solver.lookup("turbo.gpu.release"), other_flags={"-version": "1.2.0", "-hardware" : "AMD EPYC 7452 32-Core@2.35GHz; RAM 512GO;NVIDIA A100 40GB HBM"})],
+    sbatch_config="slurm_config.sh"
+)
