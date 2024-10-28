f2046bc7989077a1355a63109c9b8ff4fc86c22e
Pierre Talbot <pierre.talbot@uni.lu>
2024-10-28 09:51:23 +0100

    memkill

---
+++ compile-helios.sh
@@ -0,0 +1,23 @@
+#!/bin/bash -l
+#SBATCH --time=03:00:00
+#SBATCH -p gpu
+#SBATCH -A tutorial
+#SBATCH --gres=gpu:1
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1 # 4 GPUs so 4 tasks per nodes.
+#SBATCH --mem=0
+#SBATCH --qos=normal
+#SBATCH --export=ALL
+#SBATCH --output=slurm-compileturbo-gpu.out
+
+# Exits when an error occurs.
+set -e
+set -x # useful for debugging.
+
+source $1
+
+#cd ~/lattice-land/turbo/build/gpu-release-local
+#ptxas -arch=sm_90 -v --suppress-stack-size-warning --allow-expensive-optimizations false -O0  "turbo.ptx"  -o "turbo.cubin"
+cmake --workflow --preset gpu-release --fresh
+exit 0
+
