5fbd1259eafbe11b9f765febf4aa75dd54849297
Pierre Talbot <pierre.talbot@uni.lu>
2025-03-21 04:50:36 +0100

    clean benchmarking repo, has been replaced by lattice-land/bench a while ago.

--- benchmarks/analyzing/mznbench_check.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-mzn-bench check-solutions $1 &&
-mzn-bench check-statuses $1
--- benchmarks/analyzing/mznbench_collect.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-mzn-bench collect-objectives $1 ../campaign/$(basename $1)-objectives.csv
-mzn-bench collect-statistics $1 ../campaign/$(basename $1).csv
-
